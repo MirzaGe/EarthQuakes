6842344427c8927fda48b314cefe69c28747b369
Sherry1481 <shareyscorpi1@gmail.com>
2021-08-26 01:59:19 +0500

    Initial Commit

---
+++ EarthQuakes/EarthQuakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthQuakesApp.swift
+//  EarthQuakes
+//
+//  Created by sherry on 26/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct EarthQuakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
